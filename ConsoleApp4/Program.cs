7ddf9edfe64a01c2039d9e246769dc7120c0f1c4
marco villanueva <villanuevamarco98@gmail.com>
2020-10-07 20:43:04 -0600

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        class Car
+        {
+            public string model;
+            public string color;
+            public int year;
+
+
+            public Car(string modelName, string carColor, int carYear)
+            {
+                model = modelName;
+                color = carColor;
+                year = carYear;
+
+            }
+
+            static void Main(string[] args)
+            {
+                Car BMW = new Car("320i", "midnight", 2008);
+
+                Console.WriteLine(BMW.model +" "+ BMW.color +" "+ BMW.year);
+            }
+        }
+    }
+}
