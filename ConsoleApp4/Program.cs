8e2b6a6b99ee4cfc21e4c87e2598dfe76c8fa9e6
marco villanueva <villanuevamarco98@gmail.com>
2020-10-07 20:46:45 -0600

    constructor paramaters

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,13 +4,15 @@ namespace ConsoleApp4
 {
     class Program
     {
+        //Constructors can also take parameters, which is used to initialize fields.
         class Car
         {
             public string model;
             public string color;
             public int year;
 
-
+            //The following example adds a string modelName parameter to the constructor
+            // Create a class constructor with a parameter
             public Car(string modelName, string carColor, int carYear)
             {
                 model = modelName;
